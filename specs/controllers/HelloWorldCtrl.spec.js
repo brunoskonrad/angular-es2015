a4dba9a93aa7b08e4b523880ab3be798900fd24b
Bruno Konrad <bruno.skon@gmail.com>
2015-10-25 23:35:27 -0200

    Test the HelloWorld controller

--- specs/controllers/HelloWorldCtrl.spec.js
+++ specs/controllers/HelloWorldCtrl.spec.js
@@ -1,15 +1,39 @@
-describe('HelloWorldCtrl', () => {
-  let subject;
+let subject;
+let $controller;
+let $rootScope;
+let scope;
+let Foo;
 
+describe('HelloWorldCtrl', () => {
   beforeEach(angular.mock.module('hello-world.controllers'));
 
+  beforeEach(angular.mock.inject((_$controller_, _Foo_, _$rootScope_) => {
+    $controller = _$controller_;
+    $rootScope = _$rootScope_;
+    Foo = _Foo_;
+  }));
+
   beforeEach(() => {
-    // angular.mock.inject((HelloWorldCtrl) => {
-    //   subject = HelloWorldCtrl;
-    // });
+    scope = $rootScope.$new();
+    subject = $controller('HelloWorldCtrl', {
+      $scope: scope,
+      Foo
+    });
   });
 
   it('expect that the controller is defined', () => {
-    expect(subject).not.toBeDefined();
+    expect(subject).toBeDefined();
+  });
+
+  it('get the `hello` property as `world`', () => {
+    expect(scope.hello).toBe('world');
+  });
+
+  it('access the `foo` property', () => {
+    spyOn(Foo, 'getFoo').and.returnValue('foo');
+
+    scope.$digest();
+
+    expect(scope.foo).toBe('foo');
   });
 });
